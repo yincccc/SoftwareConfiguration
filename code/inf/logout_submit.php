8b033c6cc4f9a8e3ddc06e4754448c8a2b8b8462
whoknowgyk <770574503@qq.com>
2017-05-23 18:11:53 +0800

    代码修改
    部分代码出现问题 修改后重新提交

--- code/inf/logout_submit.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();?>
-
-<?php
-	$_SESSION["id"]=NULL;
-	$_SESSION["name"]=NULL;
-?>
--- code/js/detail.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 高凯辉 on 2017-05-26.
- */
-
-$(document).ready(function(){
-
-
-    $("#footer").load("../pieces/footer.html");
-})
