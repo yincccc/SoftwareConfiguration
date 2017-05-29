ce1a257d773bf5ea90b047dd7fea212ea29f4a69
PaulGao <18210061639@163.com>
2017-05-29 17:05:58 +0800

    utils文件

---
+++ code/inf/utils/getPrivilege.php
@@ -0,0 +1,33 @@
+﻿<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: 高凯辉
+ * Date: 2017-05-25
+ * Time: 19:56
+ */
+
+/**
+ * 必须在require "sql.php"之后调用
+ */
+function getPrivilege(){
+
+    //获取privilege,区分是市用户还是省用户
+    $sql="select * from user where id='".$_SESSION["id"]."'";
+    $result=mysql_query($sql);
+    $row=mysql_fetch_array($result);
+
+    return $row["privilege"];
+}
+
+/**
+ * 必须在require "sql.php"之后调用
+ */
+function getAdminCity(){
+
+    $sql="select * from city where user_id='".$_SESSION["id"]."'";
+    $result=mysql_query($sql);
+    $row=mysql_fetch_array($result);
+
+    return $row["id"];
+}
